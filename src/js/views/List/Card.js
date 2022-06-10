d8e84ae59821d06143a16ea85751bc386c0523fb
Al <alpalma95@gmail.com>
2022-06-10 10:03:16 +0000

    sync between state of the buttons and favorite list

--- src/js/views/List/Card.js
+++ src/js/views/List/Card.js
@@ -7,8 +7,12 @@ const Card = ({sectionTitle, cardTitle, id, imgId}) => {
   
     const {store, actions} = useContext(Context);
 
+    
+    const [isActive, setIsActive] = useState(store.favorites.some(x => x.id == id))
 
-    const [isActive, setIsActive] = useState(false)
+    useEffect(() => {
+        if (store.favorites.find(x => x.id == id) == undefined) setIsActive(false);
+    }, [store.favorites])
 
     const cardType = sectionTitle;
 
@@ -28,14 +32,6 @@ const Card = ({sectionTitle, cardTitle, id, imgId}) => {
         :
         `/planets/${id}`
     );
-
-
-    let idBtnBase = (
-        cardType == "Characters" ?
-        `c-${id}`
-        :
-        `p-${id}`
-    )
 
     const setIsActiveHandler = () => {
         if (!isActive) {
@@ -57,7 +53,7 @@ const Card = ({sectionTitle, cardTitle, id, imgId}) => {
                 <Link to={routerLink}>
                     <button className="btn btn-outline-primary">Learn more!</button>
                 </Link>
-                <button id={idBtnBase} className={`btn btn-outline-warning ms-auto ${isActive ? "active" : ""}`} onClick={setIsActiveHandler}>
+                <button name={id} className={`btn btn-outline-warning ms-auto ${isActive ? "active" : ""}`} onClick={setIsActiveHandler}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                 </svg>

86583f91aa392b722a60343a47a33ef7fe034ab8
Al <alpalma95@gmail.com>
2022-06-10 10:18:54 +0000

    implemented links and favorites empty option

--- src/js/views/Dropdown/Dropdown.js
+++ src/js/views/Dropdown/Dropdown.js
@@ -7,23 +7,27 @@ const Dropdown = () => {
 
     const {store, actions} = useContext(Context);
 
-
     return (
         <div className="dropdown">
             <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
                 Favorites <span className="badge bg-secondary rounded-circle">{store.favorites.length}</span>
             </button>
             <ul className="dropdown-menu p-1" aria-labelledby="dropdownMenuButton1">
-                {[...store.favorites].map((x, i) => (
-
-                    <li key={i}> 
-                        <p className="d-flex justify-content-between">
-                        {x.name}
-                        <span id={x.id} onClick={actions.removeFavorite}>
-                            🗑️
-                        </span>
-                        </p> 
-                    </li>
+                {
+                    !store.favorites.length ?
+                    `Empty! Add favorites!`
+                    :
+                    [...store.favorites].map((x, i) => (
+                        <li className="d-flex justify-content-between" key={i}> 
+                            <Link to={x.id.includes('p') ? `/planets/${x.id}` : `people/${x.id}`}>
+                                <p className="">
+                                {x.name}
+                                </p>
+                            </Link>
+                            <span id={x.id} onClick={actions.removeFavorite}>
+                                🗑️
+                            </span>
+                        </li>
             ))}
             </ul>
         </div>
